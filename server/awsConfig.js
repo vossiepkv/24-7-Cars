600cae80098470b8a86eeb1d90a90f062ce25e9b
vossiepkv <vossiepkv@gmail.com>
2025-01-07 20:24:33 +1100

    fixing post function

--- server/awsConfig.js
+++ server/awsConfig.js
@@ -1,14 +1,17 @@
 // src/s3Config.js
-import AWS from 'aws-sdk';
+import { S3Client } from "@aws-sdk/client-s3"; // v3 import
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './server/.env' }); // Load environment variables
 
-// Initialize and export S3 client
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION, 
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,  // Specify the appropriate region
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
+
 
 export default s3;
