2ddd54a3cc2859c773d280fe79329d6b786b5dd8
vossiepkv <vossiepkv@gmail.com>
2025-01-07 23:18:04 +1100

    designing post layout

--- src/DisplayPosts.jsx
+++ src/DisplayPosts.jsx
@@ -32,7 +32,9 @@ const DisplayPosts = () => {
         {posts.length > 0 ? (
           [...posts].reverse().map((post) => (
             <div key={post._id} className="post">
-              <h3>{post.user.name}</h3>
+              <div className='userbar'>
+                <li><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="avatar"><path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" /></svg><h3>{post.user.name}</h3></li>
+              </div>
               {/* Conditionally render the image only if mediaUrl exists */}
               {post.mediaUrl && (
                 <img
