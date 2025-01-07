c3575be4cded75af5bffa31cdc239da0624294b2
vossiepkv <vossiepkv@gmail.com>
2025-01-07 18:33:53 +1100

    render deployment

--- server/upload.js
+++ server/upload.js
@@ -1,6 +1,6 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
-import s3 from '../src/s3Config.js';
+import s3 from './s3Config.js';
 
 const upload = multer({
   storage: multerS3({
